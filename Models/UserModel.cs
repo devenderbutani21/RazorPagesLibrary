ee5a3f2a26bced97a8762f905628bcca48cbc437
devenderbutani21 <devenderbutani21@gmail.com>
2024-01-05 12:14:38 -0500

    User Model Created

---
+++ Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesLibrary.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string LibCardNo { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$")]
+        public string PhoneNumber { get; set; }
+
+        public BookModel[] Books { get; set; } = null;
+
+    }
+}
--- obj/Debug/net8.0/RazorPagesLibrary.AssemblyInfo.cs
+++ obj/Debug/net8.0/RazorPagesLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf517c9e4f54d89073937ccbf1b11ea003c09f1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d126d60189f350f33ad578142d8f0d110c02e2f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
